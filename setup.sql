148334f8ed6fd6884ef360c224fe11cf8aeb5977
ewang212 <143996102+ewang212@users.noreply.github.com>
2025-03-18 20:28:59 -0700

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,6 +1,8 @@
 -- clean up old tables
 -- must drop tables with foreign keys first 
 -- due to referential integrity constraints
+DROP VIEW IF EXISTS sales_summary_by_age_group;
+
 DROP TABLE IF EXISTS customer_visits;
 DROP TABLE IF EXISTS purchase;
 DROP TABLE IF EXISTS popularity;
@@ -8,9 +10,6 @@ DROP TABLE IF EXISTS inventory;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS store;
-DROP TABLE IF EXISTS client;
-DROP TABLE IF EXISTS admin;
-DROP VIEW sales_summary_by_age_group;
 
 -- represents customers uniquely identified by customer_id
 -- requires all non-null values
@@ -148,61 +147,5 @@ CREATE TABLE customer_visits (
 -- create index on the product_price_usd of inventory table
 CREATE INDEX idx_store_inventory_price ON inventory(product_price_usd);
 
--- view summarizes the total sales 
--- grouped by product category and customer age range.
-CREATE VIEW sales_summary_by_age_group AS
-SELECT
-    -- product category
-    product.product_category,
-    -- sort customer age into specific group
-    CASE
-    WHEN customer.age BETWEEN 0 AND 9 THEN '0-9'
-    WHEN customer.age BETWEEN 10 AND 20 THEN '10-20'
-    WHEN customer.age BETWEEN 20 AND 29 THEN '20-29'
-    WHEN customer.age BETWEEN 30 AND 39 THEN '30-39'
-    WHEN customer.age BETWEEN 40 AND 49 THEN '40-49'
-    WHEN customer.age BETWEEN 50 AND 59 THEN '50-59'
-    WHEN customer.age BETWEEN 60 AND 69 THEN '60-69'
-    WHEN customer.age BETWEEN 70 AND 79 THEN '70-79'
-    WHEN customer.age BETWEEN 80 AND 89 THEN '80-89'
-    ELSE '90+'
-    END AS age_range,
-    
-    -- Calculate total sales by summing the sale price for each purchase
-    SUM(
-    get_sale_price
-    (purchase.purchase_id, purchase.product_id, 
-    purchase.store_id, purchase.customer_id)) AS total_sales
-FROM purchase 
-JOIN product ON purchase.product_id = product.product_id
-JOIN customer ON purchase.customer_id = customer.customer_id
-GROUP BY product.product_category, age_range
-ORDER BY product.product_category, age_range;
-
-
--- create the client table
-CREATE TABLE client (
-    -- unique identifier for each client 
-    username          VARCHAR(20) PRIMARY KEY,
-    -- unique contact email for the client
-    contact_email     VARCHAR(255) UNIQUE NOT NULL,
-    -- indicates if client is a store manager
-    is_store_manager  BOOLEAN NOT NULL DEFAULT 0, 
-    -- optional phone number for source of client contact
-    phone_number      VARCHAR(20), 
-    FOREIGN KEY(username) REFERENCES user_info(username)
-    ON UPDATE CASCADE ON DELETE CASCADE
-);
-
--- create the admin table
-CREATE TABLE admin (
-    -- unique identifier for each admin
-    username         VARCHAR(20) PRIMARY KEY, 
-    -- admin role in organization 
-    -- options: researcher, engineer, scientist
-    employee_type  ENUM('researcher', 'engineer', 'maintenance') NOT NULL, 
-    FOREIGN KEY(username) REFERENCES user_info(username) 
-    ON UPDATE CASCADE ON DELETE CASCADE
-);
 
 SET FOREIGN_KEY_CHECKS = 1;
